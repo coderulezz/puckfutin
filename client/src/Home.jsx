ce1058d265274ee3ae47275a5224c6375e7b7e13
coderulezz <coderulez@protonmail.com>
2022-03-06 11:10:30 +0000

    Add footer links in the main page

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -12,6 +12,29 @@ const Home = ({ onStartClaim }) => (
 
     <img src="rainbow.png" alt="Rainbow" className="image" style={{ width: '65%' }} />
     <img src="futin.png" alt="Futin" className="image" style={{ width: '45%' }} />
+    <div className="fixed-bottom">
+      <div className="row disclaimer">
+        <div className="col-md-3">
+          <a
+            href="https://etherscan.io/address/0xec4e62080ac205539f3096e642dd3a7acd4665d4"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: 15, marginRight: 15 }}
+          >
+            Contract
+          </a>
+          |
+          <a
+            href="https://twitter.com/fputin_xyz/status/1500414606665457666"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ marginLeft: 15, marginRight: 15 }}
+          >
+            WTF is this?
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
